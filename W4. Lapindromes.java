9d07c94a79d8ab3d51544706352c9b1ef348f8b6
vtu29444-Vasu-K <vtu29444@veltech.edu.in>
2026-09-11 16:34:52 +0530

    Implement Lapindrome checker in Java

---
+++ W4. Lapindromes.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            String s = sc.next();
+            int n = s.length();
+            int mid = n / 2;
+            int[] left = new int[26];
+            int[] right = new int[26];
+            // Count characters in the left half
+            for (int i = 0; i < mid; i++) {
+                left[s.charAt(i) - 'a']++;
+            }
+            // Count characters in the right half
+            // For odd length, skip the middle character
+            for (int i = (n + 1) / 2; i < n; i++) {
+                right[s.charAt(i) - 'a']++;
+            }
+            // Compare frequencies
+            if (Arrays.equals(left, right)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
