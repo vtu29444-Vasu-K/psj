a09f50a6d3b19a86183a1e7478b60473ea462e23
vtu29444-Vasu-K <vtu29444@veltech.edu.in>
2026-09-15 20:28:53 +0530

    Create W5.Maximum Subarray.java

---
+++ W5.Maximum Subarray.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // Decide to extend the current subarray or start fresh from nums[i]
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
