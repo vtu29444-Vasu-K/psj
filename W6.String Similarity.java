2cd6b7049f8b62d83694182f7e80d7f319d9b648
vtu29444-Vasu-K <vtu29444@veltech.edu.in>
2026-09-18 09:25:44 +0530

    Create W6.String Similarity.java

---
+++ W6.String Similarity.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Solution {
+    public static long stringSimilarity(String s) {
+        int n = s.length();
+        long totalSimilarity = n;
+        int[] z = new int[n];
+        int l = 0, r = 0;
+        for (int i = 1; i < n; i++) {
+            if (i <= r) {
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            }
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }  
+            totalSimilarity += z[i];
+        }  
+        return totalSimilarity;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int t = scanner.nextInt();
+            while (t-- > 0) {
+                String s = scanner.next();
+                System.out.println(stringSimilarity(s));
+            }
+        } 
+        scanner.close();
+    }
+}
